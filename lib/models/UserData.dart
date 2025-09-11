a9c8c34484078b9b89125ef468aa31f632025f50
supekarsuraj <surajsupekar2323@gmail.com>
2025-09-11 22:41:31 +0530

    catagory
    catagory

--- lib/models/UserData.dart
+++ lib/models/UserData.dart
@@ -1,8 +1,6 @@
-// user_model.dart
 class UserData {
   final String customerName;
   final String email;
-  final String password;
   final String pincode;
   final String city;
   final String state;
@@ -11,19 +9,16 @@ class UserData {
   UserData({
     required this.customerName,
     required this.email,
-    required this.password,
     required this.pincode,
     required this.city,
     required this.state,
     required this.mobileNo,
   });
 
-  // Convert UserData to a map for API requests or storage
   Map<String, String> toMap() {
     return {
       'customer_name': customerName,
       'email': email,
-      'password': password,
       'pincode': pincode,
       'city': city,
       'state': state,
@@ -31,12 +26,10 @@ class UserData {
     };
   }
 
-  // Factory method to create UserData from a map (e.g., API response)
   factory UserData.fromMap(Map<String, dynamic> map) {
     return UserData(
       customerName: map['customer_name'] ?? '',
       email: map['email'] ?? '',
-      password: map['password'] ?? '',
       pincode: map['pincode'] ?? '',
       city: map['city'] ?? '',
       state: map['state'] ?? '',
